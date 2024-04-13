79e0d641b54bfe3dc5ce2bd7f8eaa24c94ace166
Mario Codes <mariocodes1993@gmail.com>
2024-04-13 13:14:36 +0200

    added adapter pattern example

---
+++ adapter_pattern/duck/MallardDuck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace adapterPattern.duck
+{
+    public class MallardDuck : Duck
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I'm flying!");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine("Quack");
+        }
+    }
+}
