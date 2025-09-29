a29537af4d02bf43f995736cb785c18b6048d14f
Mario Codes <mariocodes1993@gmail.com>
2025-09-29 10:42:35 +0200

    modified top project

---
+++ GenericBuilder/Builder/Builder.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericBuilder.Builder
+{
+    public class Builder
+    {
+        public static T Create<T>(Action<T>? perform = null) where T : new()
+        {
+            var instance = new T();
+            perform?.Invoke(instance);
+            return instance;
+        }
+    }
+}
