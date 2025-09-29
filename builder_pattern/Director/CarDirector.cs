c4f5c34b19aa2db25336edd27618e4d1e027a849
Mario Codes <mariocodes1993@gmail.com>
2025-09-29 11:03:49 +0200

    fix builderPattern

---
+++ builder_pattern/Director/CarDirector.cs
@@ -0,0 +1,25 @@
+﻿using BasicBuilder.Director.interfaces;
+using builderPattern.Builder.interfaces;
+using builderPattern.Models;
+
+namespace BasicBuilder.Director
+{
+    public class CarDirector : ICarDirector
+    {
+        public Car ConstructSportsCar(ICarBuilder builder)
+        {
+            builder.WithMake("Porsche")
+                   .WithModel("911")
+                   .WithYear(2024);
+            return builder.Build();
+        }
+
+        public Car ConstructSUV(ICarBuilder builder)
+        {
+            builder.WithMake("Toyota")
+                   .WithModel("Highlander")
+                   .WithYear(2024);
+            return builder.Build();
+        }
+    }
+}
